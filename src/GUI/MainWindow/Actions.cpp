a242db7fc5b44557200f291f5f809b158772a25a
Abraamyan Alexander <hryapusek@gmail.com>
2023-11-21 21:18:23 +0300

    new: createKillAciton function was separated

--- src/GUI/MainWindow/Actions.cpp
+++ src/GUI/MainWindow/Actions.cpp
@@ -2,6 +2,7 @@
 #include "Ui/UiMainWindow.hpp"
 #include "GUI/Model/ProcessTableModel.hpp"
 #include "Process/ProcessFetchers/Test/ProcessFetcherTest.hpp"
+#include <QtDebug>
 
 namespace details_
 {
@@ -9,13 +10,7 @@ namespace details_
     mw_(mw),
     menu_(std::make_unique< QMenu >())
   {
-    killAction_ = std::make_unique< ConditionAction >(tr("Kill"));
-    killAction_->setIcon(mw->style()->standardIcon(QStyle::SP_DialogCancelButton));
-    killAction_->addCheckFunction(
-      [mw = this->mw_]() -> bool {
-      auto selectedItems = mw->ui_->processTableView->selectionModel();
-      return !selectedItems->selectedRows().empty();
-    });
+    createKillAction();
     actionsHolder_.push_back(killAction_.get());
     menu_->addAction(killAction_.get());
   }
@@ -23,9 +18,27 @@ namespace details_
   void ActionsHolder::showMenu(const QPoint &pos)
   {
     std::ranges::for_each(actionsHolder_, [](ConditionAction *action) {
-      action->updateEnabled();
+      action->updateIsEnabledFlag();
     });
     menu_->exec(QCursor::pos());
+  }
+
+  void details_::ActionsHolder::createKillAction()
+  {
+    killAction_ = std::make_unique< ConditionAction >(tr("Kill"));
+    killAction_->setIcon(mw_->style()->standardIcon(QStyle::SP_DialogCancelButton));
+    killAction_->addCheckFunction(
+      [mw = this->mw_]() -> bool {
+      auto selectedItems = mw->ui_->processTableView->selectionModel();
+      return !selectedItems->selectedRows().empty();
+    });
+    killActShcut_ = std::make_unique< QShortcut >(QKeySequence::Delete, mw_->ui_->processTableView);
+    connect(killActShcut_.get(), &QShortcut::activated, killAction_.get(), &ConditionAction::checkAndTrigger);
+    connect(killAction_.get(), &QAction::triggered,
+      [action = this->killAction_.get()]() {
+      qDebug() << "Kill action";
+      // TODO call kill
+    });
   }
 
   ActionsHolder::~ActionsHolder()
