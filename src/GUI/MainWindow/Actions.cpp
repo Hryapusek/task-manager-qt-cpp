f33d9e65ca7dd902ddb268c3773db8c64e37a89d
Abraamyan Alexander <hryapusek@gmail.com>
2023-11-21 23:53:51 +0300

    new: kill action implemented

--- src/GUI/MainWindow/Actions.cpp
+++ src/GUI/MainWindow/Actions.cpp
@@ -3,6 +3,8 @@
 #include "GUI/Model/ProcessTableModel.hpp"
 #include "Process/ProcessFetchers/Test/ProcessFetcherTest.hpp"
 #include <QtDebug>
+#include <QMessageBox>
+#include <ranges>
 
 namespace details_
 {
@@ -35,9 +37,29 @@ namespace details_
     killActShcut_ = std::make_unique< QShortcut >(QKeySequence::Delete, mw_->ui_->processTableView);
     connect(killActShcut_.get(), &QShortcut::activated, killAction_.get(), &ConditionAction::checkAndTrigger);
     connect(killAction_.get(), &QAction::triggered,
-      [action = this->killAction_.get()]() {
+      [this]() {
       qDebug() << "Kill action";
-      // TODO call kill
+      auto selectedItems = this->mw_->ui_->processTableView->selectionModel();
+      auto selectedRows = selectedItems->selectedRows();
+      std::vector< int > pids;
+      pids.reserve(selectedRows.size());
+      for (auto modelItem : selectedRows)
+      {
+        auto pidStr = modelItem.data(Qt::ItemDataRole::DisplayRole).toString();
+        pids.push_back(pidStr.toInt());
+      }
+      for (auto pid : pids)
+      {
+        auto res = this->mw_->processTableModel_->kill(pid);
+        if (!res)
+        {
+          auto errorMsg = tr("Can not kill the process.")
+                          .append(" ")
+                          .append(QString::fromStdString(res.error()));
+          QMessageBox::critical(this->mw_, tr("Error"), errorMsg, QMessageBox::Close);
+          continue;
+        }
+      }
     });
   }
 
