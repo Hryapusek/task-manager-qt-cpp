7938cd2ce6f9f3345bda6e088d53a985b4116689
Alexander Abraamyan <hryapusek@gmail.com>
2023-11-25 18:22:35 +0300

    feat: OS awarness added

--- src/GUI/MainWindow/ProcTableViewHelper.cpp
+++ src/GUI/MainWindow/ProcTableViewHelper.cpp
@@ -1,30 +1,40 @@
 #include "ProcTableViewHelper.hpp"
 #include "Ui/UiMainWindow.hpp"
 #include "ProcTableSortProxy.hpp"
-#include "Process/ProcessFetchers/Windows/ProcessFetcherWindows.hpp"
+#ifdef __WIN32
+  #include "Process/ProcessFetchers/Windows/ProcessFetcherWindows.hpp"
+#else
+  #include "Process/ProcessFetchers/Windows/ProcessFetcherLinux.hpp"
+#endif
 #include "Actions.hpp"
 
 namespace details_
 {
   ProcTableViewHelper::ProcTableViewHelper(MainWindow *mw) :
-    mw_(mw)
+	  mw_(mw)
   {
-    using namespace process;
-    auto fetcher = std::make_unique< ProcessFetcherWindows >();
-    mw_->processTableModel_ = std::make_unique< ProcessTableModel >(std::move(fetcher));
-    mw_->ui_->processTableView->setSelectionBehavior(QAbstractItemView::SelectionBehavior::SelectRows);
-    mw_->ui_->processTableView->setSelectionMode(QAbstractItemView::SelectionMode::ExtendedSelection);
-    mw_->ui_->processTableView->setContextMenuPolicy(Qt::ContextMenuPolicy::CustomContextMenu);
-    auto font = mw_->ui_->processTableView->font();
-    font.setPointSize(14);
-    mw_->ui_->processTableView->setFont(font);
-    mw_->procTableSortProxy_ = std::make_unique< ProcTableSortProxy >();
-    mw_->procTableSortProxy_->setSourceModel(mw_->processTableModel_.get());
-    mw_->ui_->processTableView->setModel(mw_->procTableSortProxy_.get());
-    mw_->ui_->processTableView->setSortingEnabled(true);
-    mw_->ui_->processTableView->horizontalHeader()->setSortIndicator(mw_->processTableModel_->pidColumn(), Qt::AscendingOrder);
-    mw_->ui_->processTableView->resizeColumnsToContents();
-    mw_->ui_->processTableView->setAutoScroll(false);
-    connect(mw_->ui_->processTableView, &QTableView::customContextMenuRequested, mw_->actionsHolder_.get(), &ActionsHolder::showMenu);
+	  using namespace process;
+    
+    #ifdef __WIN32
+      auto fetcher = std::make_unique< ProcessFetcherWindows >();
+    #else
+      auto fetcher = std::make_unique< ProcessFetcherLinux >();
+    #endif
+	  
+	  mw_->processTableModel_ = std::make_unique< ProcessTableModel >(std::move(fetcher));
+	  mw_->ui_->processTableView->setSelectionBehavior(QAbstractItemView::SelectionBehavior::SelectRows);
+	  mw_->ui_->processTableView->setSelectionMode(QAbstractItemView::SelectionMode::ExtendedSelection);
+	  mw_->ui_->processTableView->setContextMenuPolicy(Qt::ContextMenuPolicy::CustomContextMenu);
+	  auto font = mw_->ui_->processTableView->font();
+	  font.setPointSize(14);
+	  mw_->ui_->processTableView->setFont(font);
+	  mw_->procTableSortProxy_ = std::make_unique< ProcTableSortProxy >();
+	  mw_->procTableSortProxy_->setSourceModel(mw_->processTableModel_.get());
+	  mw_->ui_->processTableView->setModel(mw_->procTableSortProxy_.get());
+	  mw_->ui_->processTableView->setSortingEnabled(true);
+	  mw_->ui_->processTableView->horizontalHeader()->setSortIndicator(mw_->processTableModel_->pidColumn(), Qt::AscendingOrder);
+	  mw_->ui_->processTableView->resizeColumnsToContents();
+	  mw_->ui_->processTableView->setAutoScroll(false);
+	  connect(mw_->ui_->processTableView, &QTableView::customContextMenuRequested, mw_->actionsHolder_.get(), &ActionsHolder::showMenu);
   }
 }
