007fee1c431fce83dd07426a3e1b79d10cc2d086
Abraamyan Alexander <hryapusek@gmail.com>
2023-12-08 21:10:50 +0300

    refactor: unused functions removed

--- src/GUI/MainWindow/Settings/SettingsMemento/SettingsMementoCreator.hpp
+++ src/GUI/MainWindow/Settings/SettingsMemento/SettingsMementoCreator.hpp
@@ -11,11 +11,6 @@ public:
   static SettingsMemento createDefault();  
   static std::expected<SettingsMemento, QString> readMementoFromFile();
   static std::expected<void, QString> writeMementoToFile(SettingsMemento memento);
-
-private:
-  static std::expected<SettingsMemento, QString> createMementoFromQSettings();
-  static std::expected<std::unique_ptr< QSettings >, QString> readQSettingsFromFile(QString settingsPath);
-  static std::expected<void, QString> writeQSettingsToFile(std::shared_ptr< QSettings > settings, QString settingsPath);
 };
 
 #endif
